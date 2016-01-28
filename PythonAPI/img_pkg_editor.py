1e4a14e94b11c25a84810bb4f18931f55df95e8e
furaoing <furaoing@hotmail.com>
2016-01-28 14:08:29 +0800

    Added tensorflow handler into PythonApi

--- PythonAPI/img_pkg_editor.py
+++ PythonAPI/img_pkg_editor.py
@@ -2,6 +2,31 @@
 
 from img_handler_lib import img_handler
 from img_handler_lib import Actor
+from tensorflow_lib.__init__ import tf_handler
+from py_utility import system
+
+
+def tf_ref(img_pth):
+    kws = {"website", "web site", "internet site", "site"}
+    timer = system.RunningTimer()
+    tf_result = tf_handler.tf_inference(img_pth)
+    time_elapsed = timer.end()
+    print("TF Time Consumed:"+ time_elapsed.__str__())
+    print("TF result:"+tf_result.__str__())
+
+    is_chart = False
+
+    for item in tf_result:
+        label = item["label"]
+        for kw in kws:
+            if kw in label:
+                is_chart = True
+                return is_chart
+            else:
+                pass
+
+    # classify all images with tensorflow labelling "website" as chart images
+    return is_chart
 
 
 class Editor(object):
@@ -10,7 +35,7 @@ class Editor(object):
 
     def sort(self, tmp_obj):
         return sorted(tmp_obj, key=lambda obj: obj["score"], reverse=True)
-        # TODO: Guarranty the order of original images where scores are equal
+        # Guarrantied: the order of original images where scores are equal
 
     def edit(self, pkg_rep):
         return_error = None
@@ -26,6 +51,10 @@ class Editor(object):
 
             if error == 0:
                 if score >= 0:
+                    is_chart = tf_ref(img_pth)
+                    chart_punishment = 0.5 if is_chart else 0
+                    score -= chart_punishment
+                    print("score: " + score.__str__())
                     tmp_obj.append({"id": id, "score": score})
                 else:
                     pass
@@ -33,10 +62,18 @@ class Editor(object):
                 return_error = error
             else:
                 return_error = error
+                # expecting -1, indicting image read failed
 
         sorted_obj = self.sort(tmp_obj)
         sorted_id = [item["id"] for item in sorted_obj]
         return_pkg = {"error": return_error, "img_result": sorted_id}
+
+        """
+           :return code spec:
+                0 => No Error
+               -1 => Read Image Failed
+               -2 => Server End Error
+        """
 
         self.handler.close()
 
---
+++ PythonAPI/tensorflow_lib/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from tensorflow_lib.classify_image_roy_variant_web import TfHandler
+
+
+tf_handler = TfHandler()
